79a71d9a9a227c219da2b4264ebc3b04469efcaa
zhangyuxin <bobozhangyx@163.com>
2017-08-14 20:23:09 +0800

    edit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # test.py
 
+import urllib
 import urllib2
+import json
+
 from flask import Flask
 from flask import request
 
@@ -17,16 +20,16 @@ def home():
 
 @app.route('/down', methods=['POST'])
 def down():
-    method = request.form['method']
-    url = request.form['url']
-    header = request.form['header']
-    if method == 'method':
-        target_request = urllib2.Request(url, headers=header)
+    down_method = request.form['down_method']
+    down_url = request.form['down_url']
+    down_header = json.loads(request.form['down_header'])
+    if down_method == 'get':
+        target_request = urllib2.Request(down_url, headers=down_header)
         response = urllib2.urlopen(target_request)
         return response.read()
-    elif method == 'post':
-        data = request.form['data']
-        target_request = urllib2.Request(url, data, header)
+    elif down_method == 'post':
+        data = json.loads(request.form['data'])
+        target_request = urllib2.Request(down_url, urllib.urlencode(data), down_header)
         response = urllib2.urlopen(target_request)
         return response.read()
     return 'method must be defined'
